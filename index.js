f4ae9f85e1baf90e8577c75fd77b866295be0878
GalaxianMonster <72903778+GalaxianMonster@users.noreply.github.com>
2020-10-25 19:42:08 +0800

    ?

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on("ready", () => {
+console.log(`${client.user.username} is Online')
+})
+
+
+client.on('message', message => {
+  if(!message.guild || message.author.bot || !message.content.startsWith(prefix)) return;
+  const args = message.content.slice(prefix.length).trim().split(' ');
+  const command = args.shift().toLowerCase()
+  if (!client.commands.has(command)) return;
+
+try {
+	client.commands.get(command).execute(message, args, client);
+} catch (error) {
+	console.error(error);
+	message.reply('there was an error trying to execute that command!');
+}
+})
+
+client.login(process.env.TOKEN)
