57e9bb395a65a26531fe067e701c80d1f107ce40
GalaxianMonster <72903778+GalaxianMonster@users.noreply.github.com>
2020-10-25 19:58:09 +0800

    Guide here

--- commands/sample.js
+++ commands/sample.js
@@ -1,1 +1,9 @@
-
+module.exports = {
+  name: 'sample', // name here. If you execute it will be like this: (prefix)sample
+  description: 'a sample command',
+  async execute(message, args) {
+  // your codes here
+  message.channel.send('A sample command')
+  // Until here
+}
+}
